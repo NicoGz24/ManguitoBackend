7b51ad8a8c6f7e28b0732b5a8b91e29c1e1b5885
NicoGz24 <nico24gz@gmail.com>
2022-10-28 11:38:38 -0300

    pruebas DAOs y datos para la bd

--- src/main/java/DAOs_implementation/UsuarioDAOimpl.java
+++ src/main/java/DAOs_implementation/UsuarioDAOimpl.java
@@ -1,12 +1,23 @@
 package DAOs_implementation;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import DAO.EMF;
 import DAOs_intefaces.UsuarioDAO;
 import model.Usuario;
+import net.bytebuddy.agent.builder.AgentBuilder.Identified.Extendable;
 
 public class UsuarioDAOimpl extends GenericDAOimpl<Usuario> implements UsuarioDAO{
 
-
+	private EntityManager em = EMF.getEMF().createEntityManager();
+	private EntityTransaction etx = em.getTransaction();
+		
 
 	public UsuarioDAOimpl() {
 		super(Usuario.class);
@@ -15,14 +26,37 @@ public class UsuarioDAOimpl extends GenericDAOimpl<Usuario> implements UsuarioDA
 
 	@Override
 	public Usuario getUsuario(String usuario) {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario usu = null;
+		try {
+			etx.begin();
+			TypedQuery<Usuario> consulta = EMF.getEMF().createEntityManager().createQuery("select u from Usuario u where usuario = :usuario ", Usuario.class);
+			etx.commit();
+			consulta.setParameter("usuario", usuario);
+			usu = (Usuario) consulta.getSingleResult();
+		}
+		catch (NoResultException e) {
+			System.out.println("No hay registro en la BD de lo solicitado");
+		}
+		catch (NonUniqueResultException e) {
+			System.out.println("Se han encontrado mas de un resultado de los solicitado, en la BD");
+		}
+		return usu;
 	}
 
 	@Override
 	public void borrarUsuarioPorNombre(String usuario) {
-		// TODO Auto-generated method stub
+			Usuario usu = this.getUsuario(usuario);
+			this.borrar(usu.getId());
+			
 		
+	}
+
+	public EntityTransaction getEtx() {
+		return etx;
+	}
+
+	public void setEtx(EntityTransaction etx) {
+		this.etx = etx;
 	}
 
 
--- src/main/java/model/Donacion.java
+++ src/main/java/model/Donacion.java
@@ -2,66 +2,42 @@ package model;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="donaciones", schema="manguito")
-public class Donacion {
+public abstract class Donacion {
 	@Id @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(name="id_donacion")
 	private int id;
-	@Column(name="valor")
 	private double valor;
-	@Column(name="cant_manguitos")
-	private int cantidadManguitos;
-	@Column(name="mensaje")
 	private	String mensaje;
-	@Column(name="fecha_de_donacion")
 	private	Date fechaDeDonacion;
-	@Column(name="medio_de_pago")
 	private String medioDePago;
-	@Column(name="beneficio_canjeado")
-	private boolean beneficionCanjeado;
-	@Column(name="id_plan")
-	private int id_plan;
 	
-	@OneToOne
-	
-	private Plan plan;
-	
-	@ManyToOne
-	
+	@ManyToOne(cascade = CascadeType.ALL)
 	private Usuario donador;
 	
 	
 	public Donacion() {
 		super();
 	}
-	public Donacion(double valorManguito,int cantidadManguitos,String mensajes,Date fechaDeDonacion,String medioDePago,Plan plan,Usuario donador, Emprendimiento emprendimiento) {
-		this.setBeneficionCanjeado(false);
-		this.setCantidadManguitos(cantidadManguitos);
+	public Donacion(double valorManguito,String mensajes,Date fechaDeDonacion,String medioDePago,Usuario donador) {
 		this.setDonador(donador);
 		this.setFechaDeDonacion(fechaDeDonacion);
 		this.setMedioDePago(medioDePago);
 		this.setMensajes(mensajes);
-		this.setPlan(plan);
 		this.setValorManguito(valorManguito);
 	}
 	
 	
-	public int getCantidadManguitos() {
-		return cantidadManguitos;
-	}
-	public void setCantidadManguitos(int cantidadManguitos) {
-		this.cantidadManguitos = cantidadManguitos;
-	}
 	public double getValorManguito() {
 		return valor;
 	}
@@ -86,18 +62,7 @@ public class Donacion {
 	public void setMedioDePago(String medioDePago) {
 		this.medioDePago = medioDePago;
 	}
-	public boolean isBeneficionCanjeado() {
-		return beneficionCanjeado;
-	}
-	public void setBeneficionCanjeado(boolean beneficionCanjeado) {
-		this.beneficionCanjeado = beneficionCanjeado;
-	}
-	public Plan getPlan() {
-		return plan;
-	}
-	public void setPlan(Plan plan) {
-		this.plan = plan;
-	}
+
 	public Usuario getDonador() {
 		return donador;
 	}
